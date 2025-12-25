3dbc4a976797df03951de443a270e74181faf968
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-24 20:14:56 -0400

    Adding to About

--- web/src/app/about/page.tsx
+++ web/src/app/about/page.tsx
@@ -10,7 +10,7 @@ export default function AboutPage() {
     <Container className="py-10 prose prose-neutral dark:prose-invert max-w-3xl">
       <h1>About</h1>
       <p>
-      I’m a software developer passionate about building everything 
+      I’m a full-stack software developer passionate about building everything 
       from low-level systems to sleek, user-facing applications. My projects range from a Redis-like database server, 
       a custom Unix shell, and a network packet sniffer to modern apps like a cross-platform fitness tracker (React Native + Firebase) 
       and a movie discovery website built with Next.js 14, TypeScript, and Tailwind CSS. I enjoy designing efficient, maintainable code, whether I’m working with raw sockets and concurrency or creating smooth, accessible UIs. My goal is to keep growing as a full-stack engineer — deepening my expertise in backend systems, distributed computing, and scalable APIs while continuing to deliver great UX on the frontend.
@@ -18,9 +18,9 @@ export default function AboutPage() {
       
       <h2>Skills</h2>
       <ul>
-        <li>Frontend: Next.js 14, React, TypeScript, Tailwind CSS, shadcn/ui</li>
+        <li>Frontend: React Native, React, TypeScript, Next.js 14,Tailwind CSS, shadcn/ui</li>
         <li>Mobile: React Native, Expo, Firebase Authentication</li>
-        <li>Backend & Systems: C/C++ (C++17), POSIX/Linux programming, Sockets, Multithreading, Protocol Parsing (RESP)</li>
+        <li>Backend & Systems: C#, ASP.NET, C/C++ (C++17), POSIX/Linux programming, Sockets, Multithreading, Protocol Parsing (RESP)</li>
         <li>Networking & Tools: Raw Socket Capture, Wireshark-style Packet Analysis, REST APIs</li>
         <li>Development: Git/GitHub, Make, Cross-Platform Builds, Vercel, CI/CD</li>
       </ul>
